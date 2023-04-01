4c51794dd895ed4091155d5a3b9b7d828391bef1
Imanda03 <asis03ktm@gmail.com>
2023-04-01 18:46:20 +0545

    eventListen

--- EventListener/MouseEventTest.java
+++ EventListener/MouseEventTest.java
@@ -2,17 +2,48 @@ package EventListener;
 
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class MouseEventTest implements MouseListener {
+    JFrame f = new JFrame("Catch me");
+    JButton b;
 
     public MouseEventTest() {
-        JFrame f = new JFrame("Catch me");
-        JButton b = new JButton("Catch me");
+        b = new JButton("Catch me");
         b.setBounds(20, 20, 150, 20);
+        b.addMouseListener(this);
         f.add(b);
         f.setSize(600, 600);
         f.setLayout(null);
         f.setVisible(true);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent arg0) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent arg0) {
+        Random radi = new Random();
+        int x = radi.nextInt(450);
+        int y = radi.nextInt(580);
+        b.setBounds(x, y, 150, 20);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent arg0) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent arg0) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent arg0) {
+    }
+
+    public static void main(String[] args) {
+        new MouseEventTest();
     }
 
 }
