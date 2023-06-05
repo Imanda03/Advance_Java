3698a970e4ab9bc668e9ad7493fd564386f4d1cf
Imanda03 <asis03ktm@gmail.com>
2023-06-05 17:49:33 +0545

    RMI

---
+++ RMI/Adder.java
@@ -0,0 +1,6 @@
+
+import java.rmi.Remote;
+
+public interface Adder extends Remote {
+    public int add(int x, int y) throws Exception;
+}
---
+++ RMI/RemoteAdder.java
@@ -0,0 +1,13 @@
+
+import java.rmi.server.*;
+
+public class RemoteAdder extends UnicastRemoteObject implements Adder {
+
+    RemoteAdder() throws Exception {
+    }
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+}
